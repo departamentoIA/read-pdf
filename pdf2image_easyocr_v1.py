13e7e107265fd67815d7e4ad1212cfd5738216d4
departamentoIA <caarteaga@asf.gob.mx>
2025-12-08 17:18:55 -0600

    First column obtained

--- pdf2image_easyocr_v1.py
+++ pdf2image_easyocr_v1.py
@@ -8,7 +8,7 @@ Version:        1.0
 Description:
 Text is obtained from a PDF file by using the pdf2image (with poppler) and easyocr libraries.
 The PDF file contains flat text and tables, all pages will be converted to images.
-A part of the imagen is cut depending on the coordinates of a text condition.
+A part of the imagen is cut depending on the coordinates of two text conditions.
 Some columns are saved in "output_path".
 Dependencies:   pdf2image==1.17.0, easyocr==1.7.2, pillow==12.0.0, numpy==2.2.6,
 poppler-25.07.0 installed.
@@ -22,37 +22,43 @@ import cv2
 
 pdf_path = "./data/raw/3. 2024-38-91E-85.pdf"
 output_path = "./data/processed/text.txt"
-text_condition = 'OG'      # Text to search for in the image
+text_condition1 = 'OG'      # Text to search for in the image
+text_condition2 = 'TFE'
+dx = 10
 
 # Create OCR reader
 reader = easyocr.Reader(['es', 'en'])
 
 
-def text_from_image(img_np: np.array, text_condition: str):
+def text_from_image(img_np: np.array, text_condition1: str, text_condition2: str) -> list:
     '''Obtain the coordinates of the bounding box of a text found in the image.'''
     results = reader.readtext(img_np)
     # results → [ [bbox, texto, prob], ... ]
-    boxes_found = []
+    boxes_found_1, boxes_found_2 = [], []
 
     # Search for text condition
     for (bbox, text, prob) in results:
-        if text.strip() == text_condition:
+        print(text)
+        if text.strip() == text_condition1:
             print(f"Text: {text} (confidence: {prob:.1f})")
-            boxes_found.append(bbox)
+            boxes_found_1.append(bbox)
+        if text.__contains__(text_condition2):
+            print(f"Text: {text} (confidence: {prob:.1f})")
+            boxes_found_2.append(bbox)
 
-    output_img = img_np.copy()
-
-    # Show image with bounding boxes
-    for bbox in boxes_found:
-        pts = np.array(bbox).astype(int)
-        cv2.polylines(output_img, [pts], isClosed=True,
-                      color=(0, 255, 0), thickness=3)
-
-    plt.figure(figsize=(8, 10))
-    plt.imshow(cv2.cvtColor(output_img, cv2.COLOR_BGR2RGB))
-    plt.axis('off')
-    plt.show()
-    return boxes_found
+    try:
+        x1 = int(boxes_found_1[0][0][0]) - dx
+        y1 = int(boxes_found_1[0][0][1])
+        x2 = int(boxes_found_2[0][0][0])
+        y2 = int(boxes_found_2[0][0][1])
+        crop = img_np[y1:, x1:x2]
+        plt.figure(figsize=(8, 10))
+        plt.imshow(cv2.cvtColor(crop, cv2.COLOR_BGR2RGB))
+        plt.axis('off')
+        plt.show()
+        return [x1, y1, x2, y2]
+    except:
+        return []
 
 
 # Convert PDF to images (for Windows)
@@ -68,9 +74,6 @@ for i, img in enumerate(imagenes_pil, start=1):
     # Convert PIL Image → NumPy for EasyOCR
     img_np = np.array(img)
     img_np = cv2.cvtColor(img_np, cv2.COLOR_RGB2BGR)
-    # Perform OCR
-    boxes = text_from_image(img_np, text_condition)
-    try:
-        print(boxes[0][0])
-    except:
-        pass
+    # Obtain coordenates
+    boxes = text_from_image(img_np, text_condition1, text_condition2)
+    print(boxes)
